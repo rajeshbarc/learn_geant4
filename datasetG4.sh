219597ef7d49ea5228c8dfc761d225b7184391fe
Rajesh Sahu <90236002+rajeshbarc@users.noreply.github.com>
2025-03-30 19:17:28 +0530

    Update datasetG4.sh
    Updated Messages

--- datasetG4.sh
+++ datasetG4.sh
@@ -1,3 +1,4 @@
 cd /G4/geant4-install/share/Geant4/data
 # download 15 data sets from Geant4 website onto this folder
 for f in *.tar.gz; do tar -xvf "$f"; done
+echo "Now you can start using Geant4!!"
